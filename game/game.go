855b7bd41978f9d110f29fe5f0f22492115e1cc7
randleem <erandle2211@gmail.com>
2025-07-18 13:04:37 +0100

    3.2- methods on structs

--- game/game.go
+++ game/game.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 type Item struct {
 	X int
 	Y int
+}
+type Player struct {
+	Name string
+	Item // Player embeds Item
+	Keys []string
 }
 
 const (
@@ -65,6 +73,53 @@ func main() {
 
 	fmt.Println(NewItem(10, 20))
 	fmt.Println(NewItem(10, 2000))
+
+	i.Move(10, 20)
+	fmt.Printf("i (move): %#v\n", i)
+
+	p1 := Player{
+		Name: "Emma",
+	}
+	fmt.Printf("p1: %+v\n", p1)
+	fmt.Println("p1.X:", p1.Item.X)
+
+	// p1 is Player - but have Item in Player so can use Move - composition or inheritance
+	p1.Move(100, 200)
+	fmt.Printf("p1: (move) %+v\n", p1)
+
+	p2 := Player{
+		Name: "Ernie",
+		Keys: []string{},
+	}
+	err := p2.Found("jade")
+	if err != nil {
+		fmt.Println("jade: ", err)
+	}
+	err = p2.Found("nickel")
+	if err != nil {
+		fmt.Println("nickel: ", err)
+	}
+	fmt.Println("keys:", p2.Keys)
+}
+
+/*Exercise:
+- Add a "Keys" field to player with a slice of strings
+- Add a Found(key string) method to player
+	- It should err if key is not one of :jade, copper, crystal
+	- It should add a key only once
+*/
+
+func (p *Player) Found(key string) error {
+	k := []string{"jade", "copper", "crystal"}
+
+	f := slices.Contains(k, key)
+	if f {
+		if len(p.Keys) < 1 {
+			p.Keys = append(p.Keys, key)
+		}
+		return nil
+	}
+	return fmt.Errorf("key not found")
 }
 
 /* ASIDE Use $#v for debugging/logging
@@ -74,3 +129,20 @@ fmt.Printf("a: %#v, b: %#v\n", a, b)
 */
 
 // https://go-proverbs.github.io/
+
+// Move moves i by delta X & delta y.
+// "i is called the reciever"
+// i is a pointer receiver - actual value not a copy
+/* Value vs pointer receiver
+- In general use value semantics
+- Try to keep same sematics on all methos
+- some cases when you have to use pointer semantics
+	- if you have a locked field
+	- if you need to mutate the struct - obcs send pointer ro change original value
+	- Decoding/unmarshalling
+*/
+
+func (i *Item) Move(dx, dy int) {
+	i.X += dx
+	i.Y += dy
+}
