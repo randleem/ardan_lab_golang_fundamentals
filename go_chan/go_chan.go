9cbfd47a04f15400ebf96e9de34b6310eaa8fdb8
randleem <erandle2211@gmail.com>
2025-10-07 11:39:31 +0100

    5.3 iterating and closing go channels:

--- go_chan/go_chan.go
+++ go_chan/go_chan.go
@@ -26,6 +26,35 @@ func main() {
 	fmt.Println(v)
 
 	fmt.Println(sleepSort([]int{20, 30, 10})) //[10,20,30]
+
+	go func() {
+		for i := range 4 {
+			ch <- i
+			// fmt.Println("closed:", ch)
+		}
+		close(ch)
+	}()
+	// When you iteratre over a channel you dont know how many things are coming in so will error unless you close the channel
+	for v := range ch {
+		fmt.Println(">>", v)
+	}
+
+	v = <-ch // ch is closed
+	fmt.Println("closed:", v)
+	v, ok := <-ch // ch is closed, ok false as channel closed
+	fmt.Println("closed:", v, "ok", ok)
+
+	/* The "for range" above does
+	for {
+		v, ok:= <- ch
+		if !ok{
+			break
+		}
+		fmt.Println(">>", v)
+	}
+		So syntactic sugar - shortcut to not needig the above code, breaks behind the scene if the channel is closed.
+	*/
+	// var ch chan int // ch is nil
 }
 
 /*
@@ -59,4 +88,10 @@ func sleepSort(values []int) []int {
 /* Channel Semantics
 - send/receive to/from a channel will block until opposite opporation(*) (until something receives from the channel)
 	- guarantee of delivery - receive set up before the send
+- receive from a closed channel will return zero without blocking
+	- use "comma ok" to check if channel was closed
+- send to a closed channel - will panic.
+- dont have to close channels in go - not a memory leak
+- closing a closed or nil channel will panic
+- send/receive to a nil channel will block forever.
 */
